b56ca9514b61677cbae56a01839445fe9e9470a8
Ela Bogucka <9089666+ebogucka@users.noreply.github.com>
2024-08-20 00:55:38 +0200

    Handle digit and operator key clicks

--- script.js
+++ script.js
@@ -15,18 +15,76 @@ function divide(a, b) {
 }
 
 function operate(operator, a, b) {
-    switch (operator) {
-        case "+":
-            return add(a, b);
-        case "-":
-            return subtract(a, b);
-        case "*":
-            return multiply(a, b);
-        case "/":
-            return divide(a, b);
-    }
+  switch (operator) {
+    case '+':
+      return add(a, b);
+    case '-':
+      return subtract(a, b);
+    case 'x':
+      return multiply(a, b);
+    case '/':
+      return divide(a, b);
+  }
 }
 
-var firstNumber = 0;
-var secondNumber = 0;
-var operator = "";
+var firstNumber = undefined;
+var secondNumber = undefined;
+var operator = '';
+var displayValue = '';
+var isAwaitingNumberInput = true;
+
+function handleDigitClick(digit) {
+  if (isAwaitingNumberInput) {
+    displayValue = '';
+    isAwaitingNumberInput = false;
+  }
+  displayValue += digit;
+  document.getElementById('display').textContent = displayValue;
+}
+
+function handleOperatorClick(operator) {
+  if (firstNumber === undefined) {
+    firstNumber = Number(displayValue);
+  } else {
+    secondNumber = Number(displayValue);
+    const result = operate(this.operator, firstNumber, secondNumber);
+    displayValue = result;
+    firstNumber = result;
+    secondNumber = undefined;
+  }
+  this.operator = operator;
+  isAwaitingNumberInput = true;
+  document.getElementById('display').textContent = displayValue;
+}
+
+function handleEqualsClick() {
+  secondNumber = Number(displayValue);
+  if (firstNumber !== undefined && secondNumber !== undefined) {
+    const result = operate(operator, firstNumber, secondNumber);
+    displayValue = result;
+    firstNumber = undefined;
+    secondNumber = undefined;
+    operator = '';
+    document.getElementById('display').textContent = displayValue;
+    isAwaitingNumberInput = true;
+  }
+}
+
+const digitButtons = document.querySelectorAll('.digitKey');
+digitButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    handleDigitClick(button.textContent);
+  });
+});
+
+const operatorButtons = document.querySelectorAll('.operatorKey');
+operatorButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    handleOperatorClick(button.textContent);
+  });
+});
+
+const equalsKey = document.querySelector('.equalsKey');
+equalsKey.addEventListener('click', () => {
+  handleEqualsClick();
+});
